71baf4facf85b9e70e8359b7e8d1f9b948842e5a
Davey <david@swiftapk.com>
2024-11-16 03:34:31 -0800

    Properly attach token to fetcher

--- src/client/use-cache-hooks.js
+++ src/client/use-cache-hooks.js
@@ -2,27 +2,26 @@ import { useCallback } from "react"
 import useSWR, { SWRConfiguration, SWRResponse } from "swr"
 import useSWRInfinite, { SWRInfiniteResponse } from 'swr/infinite'
 import { SupabaseClient, Session } from "@supabase/supabase-js"
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react"
+import { useSession, useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react"
 
 import { isExport } from "./client-utils"
 
 /**
  * Custom fetcher for SWR
- * @param {SupabaseClient} supabase - The Supabase client
- * @param {Session} session - The Supabase session
  * @param {string} url - The URL to fetch
+ * @param {string} [token] - The access token
  * @returns {Promise<any>} The fetch response
  * @throws {Error} The fetch error
  */
-const fetcher = async (supabase, session, url) => {
+const fetcher = async (url, token) => {
     const headers = {}
     let basePath = ""
 
     // Use base URL for export
     if (isExport()) {
         // Pass session access token
-        if (session) {
-            headers['Authorization'] = `Bearer ${session.access_token}`;
+        if (token) {
+            headers['Authorization'] = `Bearer ${token}`;
         }
 
         if (!url.startsWith("http")) {
@@ -37,11 +36,11 @@ const fetcher = async (supabase, session, url) => {
         // return { ...json, timestamp: Date.now() }
     } else {
         if (res.status == 401) {
-            supabase.auth.signOut()
+            // supabase.auth.signOut()
         }
 
         if (res.status == 404) {
-            return null
+            // return null
         }
 
         throw new Error(res.statusText)
@@ -50,49 +49,49 @@ const fetcher = async (supabase, session, url) => {
 
 /**
  * Wraps useSWR with custom fetcher and isLoading when provider isn't ready
- * @param {string} query - The query to fetch
+ * @param {string} url - The query to fetch
  * @param {SWRConfiguration} config - The SWR config
  * @param {boolean} infinite - Whether to use infinite scrolling
  * @returns {SWRInfiniteResponse} The SWR response
  */
-export function useInfiniteCache(query, config) {
-    const session = useSession()
-    const supabase = useSupabaseClient()
+export function useInfiniteCache(url, config) {
+    const { session, isLoading: sessionLoading } = useSessionContext()
 
     const getKey = useCallback((pageIndex, previousPageData) => {
         // reached the end
         if (previousPageData && !previousPageData.data) return null
 
         // first page, we don't have `previousPageData`
-        if (pageIndex === 0) return query
+        if (pageIndex === 0) return [url, session?.access_token]
 
         const { limit } = previousPageData
 
         // add the cursor to the API endpoint
-        return query + `&offset=${pageIndex * limit}`
-    }, [query])
+        return [url + `&offset=${pageIndex * limit}`, session?.access_token]
+    }, [url, session])
 
-    const swr = useSWRInfinite(getKey, {
-        fetcher: (url) => fetcher(supabase, session, url),
+    const swr = useSWRInfinite(!sessionLoading && url && getKey, {
+        fetcher: ([url, token]) => fetcher(url, token),
         ...config
     })
 
-    return swr
+    return { ...swr, isLoading: sessionLoading || swr.isLoading }
 }
 
 /**
  * Wraps useSWR with custom fetcher and isLoading when provider isn't ready
- * @param {string} query - The query to fetch
+ * @param {string} url - The query to fetch
  * @param {SWRConfiguration} config - The SWR config
  * @param {boolean} infinite - Whether to use infinite scrolling
  * @returns {SWRResponse} The SWR response
  */
-export function useCache(query, config) {
-    const session = useSession()
-    const supabase = useSupabaseClient()
+export function useCache(url, config) {
+    const { session, isLoading: sessionLoading } = useSessionContext()
 
-    return useSWR(query, {
-        fetcher: (url) => fetcher(supabase, session, url)
+    const swr = useSWR(!sessionLoading && url && [url, session?.access_token], {
+        fetcher: ([url, token]) => fetcher(url, token)
         , ...config
     })
+
+    return { ...swr, isLoading: sessionLoading || swr.isLoading }
 }
